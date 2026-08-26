6706e1ee0798ec4207ee611f4ea6cbecd0124aa4
Anvesha <anvi5765sha@gmail.com>
2026-08-26 15:41:14 +0530

    add project requirements

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name="intelligent_healthcare_assistant",
+    version="0.1.0",
+    author="Anvesha",
+    packages=find_packages(),
+    install_requires=[]
+)
